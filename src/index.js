08556a89c6be728290ae70068d3a5a8deeca4e26
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2020-12-18 22:08:05 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./styles.css";
 
 const API_URL = "http://ip-api.com/json/";
+
+fetch(API_URL).then((response) => response.blob());
+
+let img = document.createElement("img");
+img.style = "position:fixed;top:10px;left:10px;width:100px";
+document.body.append(img);
+
+img.src = URL.createObjectURL(response);
+
+setTimeout(() => {
+  img.remove();
+  URL.revokeObjectURL(img.src);
+}, 3000);
