6fdf13968683fb7e773acd66ea681ccbcb21599e
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2020-12-21 17:24:19 +0800

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import "./styles.css";
 
 const API_URL = "http://ip-api.com/json/";
+const result = document.querySelector(".result");
 
-fetch(API_URL).then((response) => response.blob());
+result.innerHTML = "You are...";
+fetch(API_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    const { city, country, timezone } = data;
+    result.innerHTML = `You are in ${city} in ${country}, and time zone is ${timezone}.`;
+  })
+  .catch((e) => {
+    console.log(e);
+    result.innerHTML = "Can't find. Where you are?";
+  });
 
-let img = document.createElement("img");
-img.style = "position:fixed;top:10px;left:10px;width:100px";
-document.body.append(img);
+// let img = document.createElement("img");
+// img.style = "position:fixed;top:10px;left:10px;width:100px";
+// document.body.append(img);
 
-img.src = URL.createObjectURL(response);
+// img.src = URL.createObjectURL(response);
 
-setTimeout(() => {
-  img.remove();
-  URL.revokeObjectURL(img.src);
-}, 3000);
+// setTimeout(() => {
+//   img.remove();
+//   URL.revokeObjectURL(img.src);
+// }, 3000);
